94a6e07eb7e137187e519d8f8dd4e2a75ce0b0d8
Mikhail Kolmogorov <fenderglass@gmail.com>
2026-08-03 17:08:32 -0400

    no flipping mode fix

--- src/plots/snps_loh.py
+++ src/plots/snps_loh.py
@@ -67,13 +67,9 @@ def snps_df_loh(args, thread_pool, df_coverage_data):
         logger.info('Getting BAFs based on VAF score')
         df_snps_in_csv = get_vafs_from_tumor_phased_vcf(df_snps_in_csv, df_coverage, chroms, args)
     else:
-        #if args.quick_start:
-        #    get_snp_segments(args, args.target_bam[0], thread_pool)
-        #    df_snps_frequencies = csv_df_chromosomes_sorter(args.out_dir_plots+'/data/snps_frequencies.csv', ['chr', 'pos', 'freq_value_a', 'hp_a', 'freq_value_b', 'hp_b'])
-        #else:
         if args.phaseblock_flipping_disable:
             logger.info('Loading and sorting SNPs df')
-            df_snps_frequencies = csv_df_chromosomes_sorter(args.out_dir_plots+'/data/snps_frequencies.csv', ['chr', 'pos', 'freq_value_a', 'hp_a', 'freq_value_b', 'hp_b'])
+            df_snps_frequencies = csv_df_chromosomes_sorter(args.out_dir_plots+'/coverage_data/snps_frequencies.csv', ['chr', 'pos', 'freq_value_a', 'hp_a', 'freq_value_b', 'hp_b'])
         else:
             logger.info('Loading and sorting SNPs df')
             df_snps_frequencies = csv_df_chromosomes_sorter(args.out_dir_plots+'/data_phasing/snps_frequencies.csv', ['chr', 'pos', 'freq_value_a', 'hp_a', 'freq_value_b', 'hp_b'])
