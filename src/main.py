2c11ff7b86c695676afbce238257290b26c15600
Tanveer Ahmad <tahashmi@users.noreply.github.com>
2026-09-02 09:21:08 -0400

    creating compressed output file for HiScanner plots

--- src/main.py
+++ src/main.py
@@ -714,8 +714,8 @@ def cna_process(args):
                     logger.info(f"Solution symbolic link created: {link_path} -> {target_folder}")
                 except OSError as e:
                     logger.info(f"Error creating symbolic link: {e}")
-
-    create_hiscanner_plot_data_archives(args.out_dir_plots, args.genome_name, args.breakpoints, args.centromere)
+            
+            create_hiscanner_plot_data_archives(args.out_dir_plots, args.genome_name, args.breakpoints, args.centromere)
     #if average_p_value:
     #    #SNPs ratios and LOH and plots
     #    plot_snps_ratios_genome(args, df_snps_in_csv, loh_regions)
